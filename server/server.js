c3bc34f62ca0f678def8b7130ab51cba6efd0de5
Ebenezer Gasonoo <gas24001@byui.edu>
2025-05-25 03:04:02 +0000

    fixed crash log issue

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 const authRoutes = require('./routes/auth');
-const auth = require('./middleware/auth');
+const { auth } = require('./middleware/auth');
 
 const app = express();
 const port = process.env.PORT || 5000;
